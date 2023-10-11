fa4615bfc885bc03a0debd695528cf033a73bef0
abdulkareemsasah <abdulkareemsasah@gmail.com>
2023-10-11 18:44:57 +0300

    disable editors

--- components/blocks/dynamic.tsx
+++ components/blocks/dynamic.tsx
@@ -58,18 +58,21 @@ const DynamicForm: React.FC<DynamicFormProps> = ({field, loading}) => {
                         </CardHeader>
                             <CardContent className={"w-full"}>
                                 {f === "notion editor" &&
-                                    <NotionEditor
-                                        value={field?.value[index]?.content ? JSON.parse(field.value?.[index]?.content) : ""}
-                                        onChangeValue={newValue => {
-                                            field.value[index] = {
-                                                type : "notion editor",
-                                                content: newValue
-                                            }
-                                        }}
-                                    />
+                                    <> 
+                                    {/* // <NotionEditor
+                                    //     value={field?.value[index]?.content ? JSON.parse(field.value?.[index]?.content) : ""}
+                                    //     onChangeValue={newValue => {
+                                    //         field.value[index] = {
+                                    //             type : "notion editor",
+                                    //             content: newValue
+                                    //         }
+                                    //     }}
+                                    // /> */}
+                                    </>
                                 }
                                 {f === "quill text editor" &&
-                                    <QuillEditor
+                                <>
+                                {/* <QuillEditor
                                         name={"editor"+index}
                                         value={field?.value[index]?.content ? field?.value[index]?.content : ""}
                                         onChange={newValue => {
@@ -78,7 +81,9 @@ const DynamicForm: React.FC<DynamicFormProps> = ({field, loading}) => {
                                                 content: newValue
                                             }
                                         }}
-                                    />
+                                    /> */}
+                                </>
+                                    
                                 }
                             </CardContent>
                     </Card>
